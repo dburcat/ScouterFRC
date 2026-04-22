bc08040deab4b916104e0cdbe4076f86aa541c5f
david <dburcad@gmail.com>
2026-04-22 14:48:06 -0400

    crud get endpoints

--- backend/app/routers/events.py
+++ backend/app/routers/events.py
@@ -1,5 +1,7 @@
-from models import Event
+from models import Event, Match, Team, RobotPerformance
 from schema.event_schema import Event_schema
+from schema.match_schema import Match_schema
+from schema.team_schema import Team_schema
 from db.session import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -17,4 +19,14 @@ def get_event(event_id: int, db: Session = Depends(get_db)):
         if event_obj is None:
                 raise HTTPException(status_code=404, detail="Event not found")
         return event_obj
+
+@event_router.get("/{event_id}/matches", response_model=list[Match_schema])
+def get_event_matches(event_id: int, db: Session = Depends(get_db)):
+        matches = db.query(Match).filter(Match.event_id == event_id).all()
+        return matches
+
+@event_router.get("/{event_id}/teams", response_model=list[Team_schema])
+def get_event_teams(event_id: int, db: Session = Depends(get_db)):
+        teams = db.query(Team).join(RobotPerformance).join(Match).filter(Match.event_id == id).distinct().all()
+        return teams
 
--- backend/app/routers/teams.py
+++ backend/app/routers/teams.py
@@ -1,5 +1,6 @@
-from models import team
+from models import Team, Match, RobotPerformance
 from schema.team_schema import Team_schema
+from schema.match_schema import Match_schema
 from db.session import get_db
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
@@ -8,12 +9,17 @@ team_router = APIRouter(prefix="/teams", tags=["teams"])
 
 @team_router.get("/", response_model=list[Team_schema])
 def get_teams(db: Session = Depends(get_db)):
-    teams = db.query(team.Team).all()
+    teams = db.query(Team).all()
     return teams
 
 @team_router.get("/{team_id}", response_model=Team_schema)
 def get_team(team_id: int, db: Session = Depends(get_db)):
-    team_obj = db.query(team.Team).filter(team.Team.team_id == team_id).first()
+    team_obj = db.query(Team).filter(Team.team_id == team_id).first()
     if team_obj is None:
         raise HTTPException(status_code=404, detail="Team not found")
     return team_obj
+
+@team_router.get("/{team_id}/matches", response_model=list[Match_schema])
+def get_team_matches(team_id: int, db: Session = Depends(get_db)):
+    matches = db.query(Match).join(RobotPerformance).filter(RobotPerformance.team_id == team_id).all()
+    return matches
