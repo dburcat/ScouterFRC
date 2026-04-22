c4b326cdac056df66909f7e8f5c8656923a932d6
david <dburcad@gmail.com>
2026-04-22 16:39:04 -0400

    initial password secuirty

--- backend/app/crud/crud_user.py
+++ backend/app/crud/crud_user.py
@@ -8,3 +8,11 @@ def get_users(db: Session):
 def get_user(user_id: int, db: Session):
         user_obj = db.query(User).filter(User.user_id == user_id).first()
         return user_obj
+
+def create_user(user: User, db: Session):
+        hashed_password = user.hashed_password # In a real application, you should hash the password before storing it
+        user.hashed_password = hashed_password
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+        return user
